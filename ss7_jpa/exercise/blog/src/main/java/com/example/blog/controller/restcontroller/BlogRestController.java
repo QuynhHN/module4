8f1b0189774cc6a4143a175a732af2a5004e98c5
HoangThiNhuQuynh <hoangnhuquynh.116@gmail.com>
2023-04-12 20:22:32 +0700

    update

---
+++ ss7_jpa/exercise/blog/src/main/java/com/example/blog/controller/restcontroller/BlogRestController.java
@@ -0,0 +1,2 @@
+package com.example.blog.controller.restcontroller;public class BlogRestController {
+}
---
+++ ss7_jpa/exercise/blog/src/main/java/com/example/blog/controller/restcontroller/CategoryRestController.java
@@ -0,0 +1,2 @@
+package com.example.blog.controller.restcontroller;public class CategoryRestController {
+}
